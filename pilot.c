1e0085783d544a87d529a665077f427812c550d0
Nikita <nikita16348@mail.com>
2026-03-08 22:18:34 +0700

    ауаыа

--- pilot.c
+++ pilot.c
@@ -6,6 +6,16 @@
 void pilot_init(void)
 {
     //настройка шим для моторов,прерывания на энкодерах
+
+
+    //инициализация пинов для моторов
+    gpio_init(L_MOTOR_PWM);
+    gpio_set_dir(L_MOTOR_PWM,GPIO_OUT);
+
+    gpio_init(R_MOTOR_PWM);
+    gpio_set_dir(R_MOTOR_PWM,GPIO_OUT);
+    
+    //дальше там страшная хуйня от ии.потом разберу
 }
 
 void core0_entry(void)
--- pilot.h
+++ pilot.h
@@ -1,3 +1,16 @@
 #pragma once
+
+#define L_MOTOR_PWM 6
+#define L_MOTOR_IN1 8
+#define L_MOTOR_IN2 9
+
+#define R_MOTOR_PWM 7
+#define R_MOTOR_IN1 12
+#define R_MOTOR_IN2 13
+
+#define ENCODER_L 28
+#define ENCODER_R 29
+
+
 void pilot_init(void);
 void core0_entry(void);
